e3b9d1bbbaba12dcc12161fc1f6dcb8ee5fddf8c
Kazunori <matunorigmnw@gmail.com>
2024-05-03 19:40:36 +0900

    add: 変数を作成し、ビューで繰り返し表示

--- backend/blog-backend/app/controllers/HomeController.scala
+++ backend/blog-backend/app/controllers/HomeController.scala
@@ -19,6 +19,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    val articles = Array("Title 1", "Title 2", "Title 3")
+    Ok(views.html.index(articles))
   }
 }
