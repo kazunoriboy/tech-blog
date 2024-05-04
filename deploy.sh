a5b298b867fda08ab5f49d6ff69c108734265789
Kazunori <matunorigmnw@gmail.com>
2024-05-05 00:03:12 +0900

    deployようのスクリプトを調整

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cd ~/
+cd
 
 unzip -o blog-backend-1.0-SNAPSHOT.zip
 
 rm blog-backend-1.0-SNAPSHOT.zip
 
 kill $(lsof -t -i:9000)
+
+echo $APPLICATION_SECRET
 
 blog-backend-1.0-SNAPSHOT/bin/blog-backend -Dplay.http.secret.key=${APPLICATION_SECRET} &
 
