191da73d1ac92bd68aecbdc1b4f0e4b04a679fce
Kazunori <matunorigmnw@gmail.com>
2024-05-04 19:27:22 +0900

    Add: デプロイ用のスクリプト

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/
+
+unzip -o blog-backend-1.0-SNAPSHOT.zip
+
+rm blog-backend-1.0-SNAPSHOT.zip
+
+kill $(lsof -t -i:9000)
+
+blog-backend-1.0-SNAPSHOT/bin/blog-backend -Dplay.http.secret.key=${APPLICATION_SECRET} &
+
