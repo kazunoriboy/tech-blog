794a3f282c9e1882086e676d334bb0403dcb3028
kazunoriboy <matunorigmnw@gmail.com>
2024-05-05 03:11:59 +0000

    deploy script を変更

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 cd ~/
-
-unzip -o blog-backend-1.0-SNAPSHOT.zip
+ZIP="blog-backend-1.0-SNAPSHOT.zip"
+NOW=`date +'%m%d%Y%H%M%S'`
+DIR=${ZIP%.zip}
+TARGET="${NOW}_${DIR}"
+unzip -o $ZIP
+mv $DIR "${NOW}_${DIR}"
 
 kill $(lsof -t -i:9000)
+find . -type d -name "*SNAPSHOT" | grep -v $NOW | xargs rm -r
 
-blog-backend-1.0-SNAPSHOT/bin/blog-backend -Dplay.http.secret.key=${APPLICATION_SECRET}
+$TARGET/bin/blog-backend -Dplay.http.secret.key=${APPLICATION_SECRET}
 
